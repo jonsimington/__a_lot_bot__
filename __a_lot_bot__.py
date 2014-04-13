a64e50623336bd2a1b24ee16d590ab2c5642a579
jswv9@mst.edu <jswv9@mst.edu>
2014-04-13 00:48:04 -0500

    Refactor / clean up the code
    - Queue is simply not needed -- not hitting the rate limit enough for it
      to be efficient
      - this gets rid of the timediff function, too, which was not implemented
        very well anyways

--- __a_lot_bot__.py
+++ __a_lot_bot__.py
@@ -1,10 +1,49 @@
 import praw
 from secret_settings import username, password
-import time
-import Queue
+import datetime
+import urllib2
+from requests.exceptions import HTTPError
+###############################################################################
+#
+#    __a_lot_bot__: a grammar nazi's best frand
+#
+###############################################################################
 
 # main bot driver
 def main():
+    for comment in subreddit_comments:
+        # individual words in a comment
+        words = comment.body.split()
+
+        # search for 'alot' in comments, comment if it's there
+        reply(words, comment, complete,)
+
+# replies to a comment
+def reply(words, comment, complete):
+    for word in words:
+        if word == "alot" and comment.id not in complete:
+            # try to comment snarky response
+            try:
+                comment.reply("It's 'a lot' not 'alot,' ya dingus!")
+                print "replied to comment {} at {}".format(comment.id, datetime.datetime.now())
+
+                # comment is viewed -- add comment to the complete set
+                complete.add(comment.id)
+
+            # posting too often in a subreddit
+            except praw.errors.RateLimitExceeded:
+                print "\tposting too often..."
+                pass
+
+            # 403 error, perhaps the bot is banned by the subreddit it's trying
+            # to comment to
+            except requests.exceptions.HTTPError:
+                print "\tencountered 403 error"
+                pass
+        else:
+            continue
+
+if __name__ == "__main__":
     # initialize reddit object
     r = praw.Reddit(user_agent='UNIQUE USER AGENT')
 
@@ -15,51 +54,11 @@ def main():
 
     subreddit = r.get_subreddit('all')
 
+    # get list of comments from specified subreddit
     subreddit_comments = praw.helpers.comment_stream(r, subreddit, limit=None)
-
-    # queue to store comments which are not able to be replied to due to the
-    #   rate limit
-    q = Queue.Queue(maxsize=25)
 
     # set containing already checked comments
     complete = set()
 
-    for comment in subreddit_comments:
-        # try to address queued comments
-        if not q.empty():
-            if timediff() >= 10:
-                # reply to comment at front of the queue
-                q_comment = q.get()
-                q_comment.reply("It's 'a lot' not 'alot,' ya dingus!")
-                time_last_commented = time.time()
-                complete.add(q_comment.id)
-
-        # individual words in a comment
-        words = comment.body.split()
-        for word in words:
-            if word == "alot" and comment.id not in complete:
-                # comment snarky response
-                try:
-                    comment.reply("It's 'a lot' not 'alot,' ya dingus!")
-                    print "replied to comment {} at {}".format(comment.id, time.time())
-                    time_last_commented = time.time()
-                    complete.add(comment.id)
-                except praw.errors.RateLimitExceeded:
-                    # posting too often in a subreddit
-                    # queue comment to be processed later
-                    q.put(comment)
-                    print "queued comment {} at {}".format(comment.id, time.time())
-                    print "\tqueue size is now {}".format(q.qsize())
-            else:
-                continue
-
-# calculates time in minutes between last comment and current time
-def timediff():
-    if time_last_commented != 0:
-        return (time.time() - time_last_commented) / 60
-    else:
-        return -1
-
-if __name__ == "__main__":
-    time_last_commented = 0
+    # run the driver
     main()
